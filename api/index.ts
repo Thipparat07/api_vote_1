e3ae71c836666ce7abb7d9f74479533cafed3b2e
Toey <thipparat4606@gmail.com>
2024-03-02 00:12:12 +0700

    edlt  deploy

--- api/index.ts
+++ api/index.ts
@@ -5,3 +5,5 @@ export const router = express.Router();
 // router.get('/', (req, res)=>{
 //     res.send('Method Get in index.ts');
 // });
+
+console.log('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
